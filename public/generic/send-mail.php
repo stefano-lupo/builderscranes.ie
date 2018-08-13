7fc9014a035d05031e8d12c5d64e10d93abd5fe6
Stefano <lupos@tcd.ie>
2018-08-13 15:35:56 +0100

    Fix email

--- public/generic/send-mail.php
+++ public/generic/send-mail.php
@@ -9,13 +9,17 @@ $emailAddress=$postArray[3];
 $message=$postArray[4];
 
 
-$to='lupos@tcd.ie';
+$to='info@builderscranes.ie';
 $subject='Mail from builderscranes.ie';
 
 $headers='MIME-Version: 1.0'."\r\n";
 $headers.='Content-type: text/html; charset=iso-8859-1'."\r\n";
-$headers.='From: '.$fn.' '.$ln.' <'.$emailAddress.'>'."\r\n";
 
+$headers.='From: '.$fn.' '.$ln.' <'.$emailAddress.'>'."\r\n";
+$headers.='Reply-To: '.$fn.' '.$ln.' <'.$emailAddress.'>'."\r\n";
+
+
+$headers.='Sender: Web Mail <webmail@emailer.stefanolupo.com>'."\r\n";
 /*$message = str_replace("\r\n", "<br>", $message);*/
 
 $email='
@@ -36,25 +40,5 @@ $email='
 
 //send mail to me
 mail($to,$subject,$email,$headers);
-
-//send mail to client for address book
-$headers2='MIME-Version: 1.0'."\r\n";
-$headers2.='Content-type: text/html; charset=iso-8859-1'."\r\n";
-$headers2.='From: Builder\'s Cranes  <info@builderscranes.ie>'."\r\n";
-
-
-mail($emailAddress,"Builder's Cranes Confirmation","Your message has been successfully sent.<br>Please add this contact to your address book to ensure our response will not be marked as junkmail.<br><br> Thank you!
-<br>
-------------------------------
-<br>Pascal Peate
-<br>
-Builder's Cranes
-<br>
-Phone: 086 252 41 11
-<br>
-Email: info@builderscranes.ie
-<br>
-Website: www.builderscranes.ie", $headers2);
-
 
 ?>
